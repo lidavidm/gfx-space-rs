a555aeb507dcd19c5572c4a2476ea3f3c6ae67c9
David Li <li.davidm96@gmail.com>
2016-10-09 16:54:30 -0400

    chore: play with mouse following/keyboard input

--- src/main.rs
+++ src/main.rs
@@ -39,10 +39,34 @@ pub fn main() {
     let mut prev = time::precise_time_ns();
     let mut accum = 0;
     let mut angle = 0.0;
+    let mut x = 0.0;
+    let mut y = 0.0;
+    sprite.position.x = 60.0;
+    sprite.position.y = 32.0;
+    let center_x = 64.0;
+    let center_y = 36.0;
     'outer: loop {
         for event in window.poll_events() {
             match event {
                 glutin::Event::Closed => break 'outer,
+                glutin::Event::MouseMoved(mx, my) => {
+                    let my = 720 - my;
+                    x = (mx as f32 / 1280.0) * 128.0 - view[3][0];
+                    y = (my as f32 / 720.0) * 72.0 - view[3][1];
+                    let cx = center_x - view[3][0];
+                    let cy = center_y - view[3][1];
+                    let angle = f32::atan2(y - cy, x - cx) + std::f32::consts::PI / 2.0;
+                    sprite.rotation = cgmath::Basis3::from_angle_z(cgmath::Rad { s: angle });
+                }
+                glutin::Event::KeyboardInput(glutin::ElementState::Pressed, code, _) => {
+                    match code {
+                        25 => view[3][1] -= 1.0,
+                        39 => view[3][1] += 1.0,
+                        38 => view[3][0] -= 1.0,
+                        40 => view[3][0] += 1.0,
+                        _ => {},
+                    }
+                }
                 _ => {},
             }
         }
@@ -52,10 +76,9 @@ pub fn main() {
         prev = cur;
         while accum > TICK_TIME {
             accum -= TICK_TIME;
-            sprite.position.x += 0.1;
             sprite2.rotation = cgmath::Basis3::from_angle_z(cgmath::Rad { s: angle });
             angle += std::f32::consts::PI / 40.0;
-            view[3][1] += 0.1;
+            // view[3][1] += 0.1;
         }
 
         encoder.clear(&main_color, BG_COLOR);
