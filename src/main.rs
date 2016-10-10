7f0a27f02785223aff12dca17c4b77bbb5af3e80
David Li <li.davidm96@gmail.com>
2016-10-10 09:23:16 -0400

    chore: add barrel

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,9 @@ pub fn main() {
 
     let sprite_factory = sprite::SpriteFactory::new(&mut factory);
     let texture = sprite::load_texture(&mut factory, std::path::Path::new("assets/textures/tankBlue_outline.png")).unwrap();
+    let barrel_texture = sprite::load_texture(&mut factory, std::path::Path::new("assets/textures/barrelBlue_outline.png")).unwrap();
     let mut sprite = sprite_factory.create(&mut factory, main_color.clone(), texture.clone(), 16.0, 16.0);
+    let mut barrel = sprite_factory.create(&mut factory, main_color.clone(), barrel_texture.clone(), 2.4, 5.8);
     let mut sprite2 = sprite_factory.create(&mut factory, main_color.clone(), texture.clone(), 16.0, 16.0);
     sprite.scale = 0.5;
 
@@ -43,6 +45,9 @@ pub fn main() {
     let mut y = 0.0;
     sprite.position.x = 60.0;
     sprite.position.y = 32.0;
+    barrel.position.x = 64.0 - 1.2;
+    barrel.position.y = 36.0;
+    barrel.rotation_center = cgmath::vec3(1.2, 0.0, 0.0);
     let center_x = 64.0;
     let center_y = 36.0;
     'outer: loop {
@@ -55,15 +60,15 @@ pub fn main() {
                     y = (my as f32 / 720.0) * 72.0 - view[3][1];
                     let cx = center_x - view[3][0];
                     let cy = center_y - view[3][1];
-                    let angle = f32::atan2(y - cy, x - cx) + std::f32::consts::PI / 2.0;
-                    sprite.rotation = cgmath::Basis3::from_angle_z(cgmath::Rad { s: angle });
+                    let angle = f32::atan2(y - cy, x - cx) - std::f32::consts::PI / 2.0;
+                    barrel.rotation = cgmath::Basis3::from_angle_z(cgmath::Rad { s: angle });
                 }
                 glutin::Event::KeyboardInput(glutin::ElementState::Pressed, code, _) => {
                     match code {
                         25 => view[3][1] -= 1.0,
                         39 => view[3][1] += 1.0,
-                        38 => view[3][0] -= 1.0,
-                        40 => view[3][0] += 1.0,
+                        38 => view[3][0] += 1.0,
+                        40 => view[3][0] -= 1.0,
                         _ => {},
                     }
                 }
@@ -83,7 +88,7 @@ pub fn main() {
 
         encoder.clear(&main_color, BG_COLOR);
         sprite.render(&mut encoder, proj, view);
-        sprite2.render(&mut encoder, proj, view);
+        barrel.render(&mut encoder, proj, view);
         encoder.flush(&mut device);
         window.swap_buffers().unwrap();
         device.cleanup();
