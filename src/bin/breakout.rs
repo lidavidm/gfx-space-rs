d2148784de838dc7174c49d14817b9395e53b148
David Li <li.davidm96@gmail.com>
2016-10-16 16:39:09 -0400

    chore: add skeleton of breakout game

---
+++ src/bin/breakout.rs
@@ -0,0 +1,55 @@
+extern crate cgmath;
+#[macro_use]
+extern crate gfx;
+extern crate gfx_device_gl;
+extern crate gfx_window_glutin;
+extern crate glutin;
+extern crate image;
+extern crate time;
+
+extern crate mgmm;
+
+use mgmm::types::*;
+
+use cgmath::{SquareMatrix};
+
+const BG_COLOR: [f32; 4] = [0.529, 0.808, 0.980, 1.0];
+
+const WINDOW_WIDTH: u32 = 960;
+const WINDOW_HEIGHT: u32 = 640;
+
+const WORLD_WIDTH: f32 = 960.0;
+const WORLD_HEIGHT: f32 = 640.0;
+
+type R = gfx_device_gl::Resources;
+struct Game {
+    proj: UniformMat4,
+    view: UniformMat4,
+}
+
+impl mgmm::game::Game for Game {
+    fn init(factory: &mut gfx_device_gl::Factory, main_color: &RenderTarget, _main_depth: &DepthTarget) -> Game {
+        let proj: UniformMat4 = cgmath::ortho(0.0, WORLD_WIDTH, 0.0, WORLD_HEIGHT, 0.0, 1.0).into();
+        let view: UniformMat4 = cgmath::Matrix4::identity().into();
+
+        Game {
+            proj: proj,
+            view: view,
+        }
+    }
+
+    fn tick(&mut self) {
+    }
+
+    fn handle_event(&mut self, event: &glutin::Event) {
+    }
+
+    fn render(&mut self, encoder: &mut GLEncoder, target: &RenderTarget) {
+        encoder.clear(target, BG_COLOR);
+    }
+}
+
+pub fn main() {
+    let mut game = mgmm::game::App::<Game>::new("Breakout", WINDOW_WIDTH, WINDOW_HEIGHT);
+    game.run();
+}
