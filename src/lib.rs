e1b00f26f71803882dc841d56fc0df25ebb00a97
David Li <li.davidm96@gmail.com>
2016-10-16 16:38:50 -0400

    feat: add lib.rs to use crate as library

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+extern crate cgmath;
+#[macro_use]
+extern crate gfx;
+extern crate gfx_device_gl;
+extern crate gfx_window_glutin;
+extern crate glutin;
+extern crate image;
+extern crate tiled;
+extern crate time;
+
+pub mod game;
+pub mod input;
+pub mod sprite;
+pub mod tilemap;
+pub mod types;
+pub mod player;
