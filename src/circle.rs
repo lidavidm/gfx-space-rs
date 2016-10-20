b3d0b644e9dbe998678752df670013885d08380b
David Li <li.davidm96@gmail.com>
2016-10-20 11:37:06 -0400

    feat(Graphics): use points to render Circle

--- src/circle.rs
+++ src/circle.rs
@@ -14,6 +14,7 @@ gfx_defines! {
         view: UniformMat4 = "u_View",
         model: UniformMat4 = "u_Model",
         color: [f32; 3] = "u_Color",
+        point_size: f32 = "u_PointSize",
     }
 
     pipeline pipe {
@@ -29,8 +30,6 @@ pub struct Circle<R: gfx::Resources> {
     slice: gfx::Slice<R>,
     pub position: cgmath::Vector3<f32>,
     pub scale: f32,
-    pub rotation: cgmath::Basis3<f32>,
-    pub rotation_center: cgmath::Vector3<f32>,
     pub width: f32,
     pub height: f32,
     pub color: [f32; 3],
@@ -46,21 +45,21 @@ impl<R: gfx::Resources> Circle<R> {
         where F: gfx::Factory<R> {
         let vertices = [
             Vertex { pos: [0.0, 0.0] },
-            Vertex { pos: [1.0, 0.0] },
-            Vertex { pos: [0.0, 1.0] },
-            Vertex { pos: [1.0, 1.0] },
         ];
-        let indices: [u16; 6] = [
-            0, 1, 3,
-            0, 3, 2,
+        let indices: [u16; 1] = [
+            0,
         ];
         let (vertex_buffer, slice) = factory.create_vertex_buffer_with_slice(
             &vertices, &indices as &[u16]);
-        let pso = factory.create_pipeline_simple(
+        let shader_set = factory.create_shader_set(
             include_bytes!("shader/circle_150.glslv"),
-            // TODO: https://rubendv.be/blog//opengl/drawing-antialiased-circles-in-opengl/
-            include_bytes!("shader/circle_150.glslf"),
-            pipe::new()).unwrap();
+            include_bytes!("shader/circle_150.glslf")).unwrap();
+        let pso = factory.create_pipeline_state(
+            &shader_set,
+            gfx::Primitive::PointList,
+            gfx::state::Rasterizer::new_fill(),
+            pipe::new(),
+        ).unwrap();
         let data = pipe::Data {
             vbuf: vertex_buffer,
             locals: factory.create_constant_buffer(1),
@@ -73,8 +72,6 @@ impl<R: gfx::Resources> Circle<R> {
             slice: slice,
             position: cgmath::vec3(0.0, 0.0, 0.0),
             scale: 1.0,
-            rotation: cgmath::Basis3::one(),
-            rotation_center: cgmath::vec3(0.0, 0.0, 0.0),
             width: width,
             height: height,
             color: color,
@@ -87,23 +84,17 @@ impl<R: gfx::Resources> Circle<R> {
                  view: UniformMat4)
         where C: gfx::CommandBuffer<R> {
         // TODO: cache recomputation of model matrix where possible
-        let translate_to_center = cgmath::Matrix4::from_translation(-self.rotation_center);
-        let rotation: cgmath::Matrix4<f32> = cgmath::Decomposed {
-            scale: 1.0,
-            rot: self.rotation,
-            disp: cgmath::vec3(0.0, 0.0, 0.0),
-        }.into();
-        let translate_from_center = cgmath::Matrix4::from_translation(self.rotation_center);
         let scale = cgmath::Matrix4::from_nonuniform_scale(self.scale * self.width, self.scale * self.height, 1.0);
         let translate_to_position = cgmath::Matrix4::from_translation(self.position);
 
-        let model = translate_to_position * translate_from_center * rotation * translate_to_center * scale;
+        let model = translate_to_position * scale;
 
         let locals = Locals {
             proj: proj,
             view: view,
             model: model.into(),
             color: self.color,
+            point_size: 50.0,
         };
 
         encoder.update_buffer(&self.data.locals, &[locals], 0).unwrap();
