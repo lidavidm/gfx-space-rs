e9ec3a7ff74bcba481f5578543f2dd5ac97da044
David Li <li.davidm96@gmail.com>
2016-10-30 20:25:23 -0400

    refactor(Blur): move blur into its own struct

--- src/bin/blur.rs
+++ src/bin/blur.rs
@@ -46,30 +46,16 @@ gfx_defines! {
     }
 }
 
-struct Game {
-    proj: UniformMat4,
-    view: UniformMat4,
-    circle: Circle<R>,
-
-    blur: Bundle<R, blur::Data<R>>,
+struct Blur<R: gfx::Resources> {
+    bundle: Bundle<R, blur::Data<R>>,
     rtv: gfx::handle::RenderTargetView<R, ColorFormat>,
 }
 
-impl mgmm::game::Game for Game {
-    fn init(factory: &mut gfx_device_gl::Factory, main_color: &RenderTarget, _main_depth: &DepthTarget) -> Game {
-        let proj: UniformMat4 = cgmath::ortho(0.0, WORLD_WIDTH, 0.0, WORLD_HEIGHT, 0.0, 1.0).into();
-        let view: UniformMat4 = cgmath::Matrix4::identity().into();
-
-        let (buf_width, buf_height, _, _) = main_color.get_dimensions();
+impl Blur<R> {
+    fn new<F>(factory: &mut F, target: &gfx::handle::RenderTargetView<R, ColorFormat>) -> Blur<R> where F: gfx::Factory<R> {
+         let (buf_width, buf_height, _, _) = target.get_dimensions();
         // TODO: want a non-sRGB intermediate buffer
         let (_, srv, rtv) = factory.create_render_target::<ColorFormat>(buf_width, buf_height).unwrap();
-
-        let circle = Circle::new(
-            factory,
-            rtv.clone(),
-            [1.0, 0.0, 0.0],
-            10.0,
-        );
 
         let vertices = [
             BlurVertex { pos: [0.0, 0.0], uv: [0.0, 0.0] },
@@ -91,16 +77,55 @@ impl mgmm::game::Game for Game {
             vbuf: vbuf,
             texture: (srv, sampler),
             locals: factory.create_constant_buffer(1),
-            out: main_color.clone(),
+            out: target.clone(),
         };
+        let bundle = Bundle::new(slice, pso, data);
+
+        Blur {
+            bundle: bundle,
+            rtv: rtv,
+        }
+    }
+
+    pub fn render<C>(&mut self,
+                 encoder: &mut gfx::Encoder<R, C>,
+                 proj: UniformMat4,
+                 view: UniformMat4)
+        where C: gfx::CommandBuffer<R> {
+        let locals = BlurLocals {
+            proj: proj,
+        };
+
+        encoder.update_buffer(&self.bundle.data.locals, &[locals], 0).unwrap();
+        self.bundle.encode(encoder);
+    }
+}
+
+struct Game {
+    proj: UniformMat4,
+    view: UniformMat4,
+    circle: Circle<R>,
+    blur: Blur<R>,
+}
+
+impl mgmm::game::Game for Game {
+    fn init(factory: &mut gfx_device_gl::Factory, main_color: &RenderTarget, _main_depth: &DepthTarget) -> Game {
+        let proj: UniformMat4 = cgmath::ortho(0.0, WORLD_WIDTH, 0.0, WORLD_HEIGHT, 0.0, 1.0).into();
+        let view: UniformMat4 = cgmath::Matrix4::identity().into();
+
+        let blur = Blur::new(factory, main_color);
+        let circle = Circle::new(
+            factory,
+            blur.rtv.clone(),
+            [1.0, 0.0, 0.0],
+            10.0,
+        );
 
         Game {
             proj: proj,
             view: view,
             circle: circle,
-
-            blur: Bundle::new(slice, pso, data),
-            rtv: rtv,
+            blur: blur,
         }
     }
 
@@ -113,15 +138,10 @@ impl mgmm::game::Game for Game {
     }
 
     fn render(&mut self, encoder: &mut GLEncoder, target: &RenderTarget) {
-        encoder.clear(&self.rtv, [0.0, 0.0, 0.0, 0.0]);
+        encoder.clear(&self.blur.rtv, [0.0, 0.0, 0.0, 0.0]);
         encoder.clear(target, BG_COLOR);
         self.circle.render(encoder, self.proj, self.view);
-        let locals = BlurLocals {
-            proj: self.proj,
-        };
-
-        encoder.update_buffer(&self.blur.data.locals, &[locals], 0).unwrap();
-        self.blur.encode(encoder);
+        self.blur.render(encoder, self.proj, self.view);
     }
 }
 
